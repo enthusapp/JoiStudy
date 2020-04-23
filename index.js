28eece28ab7709b754b4c8b4b799d520fc8448f1
enthusapp <enthusapp@gmail.com>
2020-04-23 22:50:09 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,30 @@
+const Joi = require('@hapi/joi');
+
+const schema = Joi.object({
+    username: Joi.string()
+        .alphanum()
+        .min(3)
+        .max(30)
+        .required(),
+
+    password: Joi.string()
+        .pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')),
+
+    repeat_password: Joi.ref('password'),
+
+    access_token: [
+        Joi.string(),
+        Joi.number()
+    ],
+
+    birth_year: Joi.number().integer().min(1900).max(2013).default(2000).failover(2001),
+
+    email: Joi.string()
+        .email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } })
+})
+    .with('username', 'birth_year')
+    .xor('password', 'access_token')
+    .with('password', 'repeat_password');
+
+
+console.log(schema.validate({ username: 'abc', password: '1234', repeat_password: '1234', birth_year: 2020 }));
